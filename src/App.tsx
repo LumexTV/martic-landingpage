c5b51d80bf67e4334a7af685030ed85d855f5ecc
LumexTV <luka_martic@web.de>
2025-07-19 13:42:15 +0200

    loading animation entfernt

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AnimatePresence } from 'framer-motion';
-import LoadingScreen from './components/LoadingScreen';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Automation from './components/Automation';
@@ -14,41 +11,25 @@ import PrivacyPolicy from './pages/PrivacyPolicy';
 import Impressum from './pages/Impressum';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 0);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <Router>
-      <AnimatePresence>
-        {isLoading && <LoadingScreen />}
-      </AnimatePresence>
-      
-      {!isLoading && (
-        <div className="bg-white min-h-screen">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={
-              <>
-                <Hero />
-                <Automation />
-                <Benefits />
-                <Security />
-                <Contact />
-              </>
-            } />
-            <Route path="/datenschutz" element={<PrivacyPolicy />} />
-            <Route path="/impressum" element={<Impressum />} />
-          </Routes>
-          <Footer />
-        </div>
-      )}
+      <div className="bg-white min-h-screen">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Hero />
+              <Automation />
+              <Benefits />
+              <Security />
+              <Contact />
+            </>
+          } />
+          <Route path="/datenschutz" element={<PrivacyPolicy />} />
+          <Route path="/impressum" element={<Impressum />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   );
 }
