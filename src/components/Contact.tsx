9f059380b8278f5ab7fcd27f9027b17fc1c480a6
LumexTV <luka_martic@web.de>
2025-07-24 23:39:05 +0200

    Improve chat widget styling and contact form input type

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -166,8 +166,8 @@ const Contact = () => {
                     <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                       <Globe className="h-5 w-5 text-gray-400" />
                     </div>
-                    <input
-                      type="url"
+<input
+                      type="text"
                       id="website"
                       name="website"
                       value={formData.website}
